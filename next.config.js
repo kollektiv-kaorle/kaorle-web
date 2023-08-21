796058bb5d54cca2d978ecc4d5ba831e08b56a46
Nicolai Benker <contact@nbenker.io>
2023-08-21 20:18:35 +0200

    add cyril cyril shortcut via next config

--- next.config.js
+++ next.config.js
@@ -7,6 +7,11 @@ const nextConfig = {
         destination: "/events",
         permanent: false,
       },
+      {
+        source: "/cyrilcyril",
+        destination: "/events/9e7d95c1-fd60-4316-b2f9-df82577057fe",
+        permanent: true,
+      },
     ];
   },
   images: {
