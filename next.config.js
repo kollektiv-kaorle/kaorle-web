396a3bd842c7d8b2ad0affc67d897230fe884ec7
Nicolai Benker <contact@nbenker.io>
2023-10-01 21:41:29 +0200

    add holy wave shortcut

--- next.config.js
+++ next.config.js
@@ -12,6 +12,11 @@ const nextConfig = {
         destination: "/events/9e7d95c1-fd60-4316-b2f9-df82577057fe",
         permanent: true,
       },
+      {
+        source: "/holywave",
+        destination: "/events/762568eb-87a1-4681-be37-f1cdc1cf4b95",
+        permanent: true,
+      },
     ];
   },
   images: {
